7e41ddbbfd76ec8163f44b00a7968681e918878d
Sergey Matuynin <sbmatyunin@gmail.com>
2015-04-05 00:05:17 +0300

    Added functions for image writing and overlay

--- utils/image.py
+++ utils/image.py
@@ -21,3 +21,15 @@ def imread(path):
         img = img[:,:,::-1]
         
     return img
+
+
+def imwrite(path, img):
+    if img.ndim == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    return cv2.imwrite(path, img)
+
+
+def overlay(fg, bg, pos_x, pos_y):
+    img = bg.copy()
+    img[pos_y:pos_y+fg.shape[0], pos_x:pos_x+fg.shape[1]] = fg
+    return img
