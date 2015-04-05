8805e13d67a7fa8e2142bb28aec22b181bbb1ce0
Sergey Matuynin <sbmatyunin@gmail.com>
2015-04-05 20:35:35 +0300

    basic depth propagation algoritm is wrapped into depth_propagation_base

---
+++ algo/depth_propagation_base.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+__author__ = 's'
+
+
+import logging
+
+import motion_estimation
+
+
+class DepthPropagation(object):
+    def __init__(self, img, dpt):
+        self.length = len(img)
+        self.img = img
+        self.dpt = dpt
+        self.res = [None,] * self.length
+        if self.length != len(self.dpt):
+            raise Exception("Length of input image sequence must match length of input depth sequence."
+                            "Use empty (None) placeholders for unknown depth frames")
+
+
+    def preprocess(self):
+        pass
+
+
+    def __getitem__(self, key):
+        return self.res[key]
+
+
+
+
+class DepthPropagationFwd(DepthPropagation):
+    def __init__(self, img, dpt):
+        super(DepthPropagationFwd, self).__init__(img, dpt)
+        self.logger = logging.getLogger(__name__)
+        self.res[0] = self.dpt[0]
+        self.flowFwd = None
+        self.motion = motion_estimation.MotionEstimation()
+
+    def preprocess_flow(self):
+        self.logger.debug("Optcal flow calculation")
+        if self.flowFwd is None:
+            self.flowFwd = [None, ] + [self.motion.calc(self.img[i], self.img[i-1])
+                                       for i in range(1, self.length)]
+
+    def compensate_fwd(self):
+        self.logger.debug("Motion compensation")
+        for i in range(1, self.length):
+            u, v = self.flowFwd[i]
+            self.res[i] = self.motion.warp(self.res[i-1], u, v)
+
+    def preprocess(self):
+        self.preprocess_flow()
+        self.compensate_fwd()
+
+
