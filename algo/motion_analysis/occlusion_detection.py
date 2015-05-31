a51cffe09638e25c1adc343ab76eb27cb876d99c
Sergey Matuynin <sbmatyunin@gmail.com>
2015-05-31 19:03:18 +0300

    Added faster implementation of covered areas detection

--- algo/motion_analysis/occlusion_detection.py
+++ algo/motion_analysis/occlusion_detection.py
@@ -1,16 +1,45 @@
 __author__ = 's'
 
 import math
-
+import numba
 import numpy
 
 
-def _update_covered(covered, i, j, coeff):
+def _update_covered_reference(covered, i, j, coeff):
     if 0 <= i < covered.shape[0] and 0 <= j < covered.shape[1]:
         covered[i, j] += coeff
 
 
 def get_covered_reference(u, v):
+    if u.shape != v.shape:
+        raise Exception("x and y components of the flow must have the same size")
+
+    covered = numpy.zeros(shape=u.shape, dtype=numpy.float32)
+    h, w = u.shape
+
+    for i in xrange(h):
+        for j in xrange(w):
+            mx, my = u[i, j], v[i, j]
+            mxf, myf = math.floor(mx), math.floor(my)
+            cx, cy = mx-mxf, my-myf
+
+            y_dst = i+int(myf)
+            x_dst = j+int(mxf)
+            _update_covered_reference(covered, y_dst,   x_dst  , (1.-cy)*(1.-cx))
+            _update_covered_reference(covered, y_dst+1, x_dst  , (   cy)*(1.-cx))
+            _update_covered_reference(covered, y_dst  , x_dst+1, (1.-cy)*(   cx))
+            _update_covered_reference(covered, y_dst+1, x_dst+1, (   cy)*(   cx))
+
+    return covered
+
+
+@numba.jit
+def _update_covered(covered, i, j, coeff):
+    if 0 <= i < covered.shape[0] and 0 <= j < covered.shape[1]:
+        covered[i, j] += coeff
+
+@numba.jit
+def get_covered(u, v):
     if u.shape != v.shape:
         raise Exception("x and y components of the flow must have the same size")
 
@@ -31,4 +60,6 @@ def get_covered_reference(u, v):
             _update_covered(covered, y_dst+1, x_dst+1, (   cy)*(   cx))
 
     return covered
+
+
 
