8596b7f90699419c5d06c37aab71d3eaca4dc1d6
Sergey Matuynin <sbmatyunin@gmail.com>
2015-06-10 00:45:41 +0300

    Clean up code of details_viewer.py

--- utils/details_viewer.py
+++ utils/details_viewer.py
@@ -7,6 +7,7 @@ import cPickle as Pickle
 import os
 import sys
 import argparse
+import logging
 
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
@@ -30,9 +31,9 @@ list_keys_accepted = [
 set_keys_accepted = set(list_keys_accepted)
 
 
-class MyDock(Dock):
+class DockWithKeyMapping(Dock):
     def __init__(self, dict_key_mapping, set_keys_accepted, *args, **kwargs):
-        super(MyDock, self).__init__(*args, **kwargs)
+        super(DockWithKeyMapping, self).__init__(*args, **kwargs)
 
         self.key_ = None
 
@@ -48,12 +49,8 @@ class MyDock(Dock):
         self.dict_key_mapping_ = dict_key_mapping
 
     def keyPressEvent(self, e):
-        # print e.modifiers(), QtCore.Qt.ControlModifier,
-        # print e.key()
-
         if e.modifiers() & QtCore.Qt.ControlModifier:
             if e.key() in self.set_keys_accepted:
-                # print "Set key {} to {}".format(e.key(), self)
                 self.set_key(e.key())
                 return  # accept
 
@@ -64,21 +61,22 @@ class MyDock(Dock):
         self.dict_key_mapping_[self.key_] = self.name()
 
 
-class MyDockArea(DockArea):
+class DockAreaWithKeyMapping(DockArea):
     _name_dict_shortcuts = 'dict_shortcuts'
 
-    def __init__(self, list_tpl_name_image, set_keys_accepted, *args, **kwargs):
-        super(MyDockArea, self).__init__(*args, **kwargs)
+    def __init__(self, dict_name_image, set_keys_accepted, *args, **kwargs):
+        logger = logging.getLogger(__name__)
+        super(DockAreaWithKeyMapping, self).__init__(*args, **kwargs)
         self.set_keys_accepted = set_keys_accepted
-        self.list_tpl_name_image = list_tpl_name_image
+        self.list_tpl_name_image = dict_name_image
 
         self.dict_key_mapping = dict()
 
         first_widget = None
         self.list_dock = []
-        for idx, (name, image) in enumerate(list_tpl_name_image.items()):
-            d = MyDock(name=name, size=(500, 200), dict_key_mapping=self.dict_key_mapping,
-                       set_keys_accepted=self.set_keys_accepted)
+        for idx, (name, image) in enumerate(dict_name_image.items()):
+            d = DockWithKeyMapping(name=name, size=(500, 200), dict_key_mapping=self.dict_key_mapping,
+                                   set_keys_accepted=self.set_keys_accepted)
 
             self.addDock(d, 'above')
 
@@ -96,62 +94,62 @@ class MyDockArea(DockArea):
             try:
                 d.set_key(list_keys_accepted[idx])
             except IndexError, e:
-                print e
+                logger.debug(str(e))
 
     def keyPressEvent(self, e):
+        logger = logging.getLogger(__name__)
         if e.modifiers() & QtCore.Qt.ControlModifier:
             e.ignore()
             return
 
         key = e.key()
-        print key,
+
         name_dock = self.dict_key_mapping.get(key, None)
-        print name_dock
+
         if name_dock is not None:
             t = self.docks[name_dock]
-            # print t, t.container()
+
             try:
                 t.raiseDock()
             except AttributeError, e:
-                print e.message
+                logger.debug(str(e))
 
     def saveState(self):
-        state = super(MyDockArea, self).saveState()
-        state[MyDockArea._name_dict_shortcuts] = copy.deepcopy(self.dict_key_mapping)
+        state = super(DockAreaWithKeyMapping, self).saveState()
+        state[DockAreaWithKeyMapping._name_dict_shortcuts] = copy.deepcopy(self.dict_key_mapping)
         return state
 
     def restoreState(self, state):
-        super(MyDockArea, self).restoreState(state)
+        logger = logging.getLogger(__name__)
+        super(DockAreaWithKeyMapping, self).restoreState(state)
         self.dict_key_mapping.clear()
-        self.dict_key_mapping.update(state[MyDockArea._name_dict_shortcuts])
+        self.dict_key_mapping.update(state[DockAreaWithKeyMapping._name_dict_shortcuts])
         for k, v in self.docks.items():
             try:
                 v.set_key_mapping(self.dict_key_mapping)
             except AttributeError, e:
-                print e.message
+                logger.debug(str(e))
 
-class MyWindow2(QtGui.QMainWindow):
 
+class DViewerWindow(QtGui.QMainWindow):
     def make_d1(self):
-        d1 = Dock("Dock1", size=(100, 10))  ## give this dock the minimum possible size
+        d1 = Dock("Dock1", size=(100, 10))  # give this dock the minimum possible size
         area = self.area
-        area.addDock(d1, 'left')  ## place d1 at left edge of dock area (it will fill the whole space since there are no other docks yet)
+        area.addDock(d1,
+                     'left')  # place d1 at left edge of dock area (it will fill the whole space since there are no other docks yet)
 
-
-        ## first dock gets save/restore buttons
         w1 = pg.LayoutWidget()
         label = QtGui.QLabel("""Save and load dock state""")
-        saveBtn = QtGui.QPushButton('Save dock state')
-        restoreBtn = QtGui.QPushButton('Restore dock state')
-        selectFileBtn = QtGui.QPushButton('Select file')
-        self.labelPathFile = QtGui.QLabel(self.path_layout_file)
+        btn_save = QtGui.QPushButton('Save dock state')
+        btn_restore = QtGui.QPushButton('Restore dock state')
+        btn_select_file = QtGui.QPushButton('Select file')
 
-        # restoreBtn.setEnabled(False)
+        # btn_restore.setEnabled(False)
         w1.addWidget(label, row=0, col=0)
-        w1.addWidget(saveBtn, row=1, col=0)
-        w1.addWidget(restoreBtn, row=2, col=0)
+        w1.addWidget(btn_save, row=1, col=0)
+        w1.addWidget(btn_restore, row=2, col=0)
         w1.addWidget(self.labelPathFile, row=3, col=0)
-        w1.addWidget(selectFileBtn, row=3, col=1)
+        w1.addWidget(btn_select_file, row=3, col=1)
         d1.addWidget(w1)
 
 
@@ -166,10 +164,10 @@ class MyWindow2(QtGui.QMainWindow):
                 with open(self.path_layout_file, "w") as f:
                     Pickle.dump(state, f)
             except IOError:
-                msgBox = QtGui.QMessageBox()
-                msgBox.setText("Failed to save file '{}'".format(self.path_layout_file))
-                msgBox.exec_()
-            # restoreBtn.setEnabled(True)
+                msg_box = QtGui.QMessageBox()
+                msg_box.setText("Failed to save file '{}'".format(self.path_layout_file))
+                msg_box.exec_()
+                # btn_restore.setEnabled(True)
 
         def load():
             if not self.path_layout_file:
@@ -183,35 +181,35 @@ class MyWindow2(QtGui.QMainWindow):
 
                 area.restoreState(state)
             except IOError:
-                msgBox = QtGui.QMessageBox()
-                msgBox.setText("Failed to open file '{}'".format(self.path_layout_file))
-                msgBox.exec_()
+                msg_box = QtGui.QMessageBox()
+                msg_box.setText("Failed to open file '{}'".format(self.path_layout_file))
+                msg_box.exec_()
 
         def select_file():
-            self.path_layout_file = QtGui.QFileDialog.getOpenFileName(caption=QtCore.QString("Select layout file"), directory=os.path.abspath("."))
+            self.path_layout_file = QtGui.QFileDialog.getOpenFileName(caption=QtCore.QString("Select layout file"),
+                                                                      directory=os.path.abspath("."))
             self.labelPathFile.setText(self.path_layout_file)
 
-        saveBtn.clicked.connect(save)
-        restoreBtn.clicked.connect(load)
-        selectFileBtn.clicked.connect(select_file)
+        btn_save.clicked.connect(save)
+        btn_restore.clicked.connect(load)
+        btn_select_file.clicked.connect(select_file)
 
     def __init__(self, dict_name_image, default_path_layout_file=None):
-        super(MyWindow2, self).__init__()
-
+        super(DViewerWindow, self).__init__()
 
         self.path_layout_file = default_path_layout_file
-        area = MyDockArea(list_tpl_name_image=list_tpl_name_image, set_keys_accepted=set_keys_accepted)
+        area = DockAreaWithKeyMapping(dict_name_image=list_tpl_name_image, set_keys_accepted=set_keys_accepted)
         self.area = area
         self.setCentralWidget(area)
         self.resize(1000, 500)
         self.setWindowTitle('Details Viewer')
 
+        self.labelPathFile = QtGui.QLabel(self.path_layout_file)
         self.make_d1()
 
 
-## Start Qt event loop unless running in interactive mode or using pyside.
+# Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
-
 
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
 
@@ -223,29 +221,17 @@ if __name__ == '__main__':
             '--layout', metavar='layout', type=str,
             nargs=1, help='Path to layout file')
         parser.add_argument(
-            '--use_short_path', type=int, default=1, choices=(0,1),
+            '--use_short_path', type=int, default=1, choices=(0, 1),
             help='Use full path as a key'
         )
         parameters = parser.parse_args(sys.argv[1:])
-        print parameters
 
-        list_tpl_name_image = { (path if parameters.use_short_path else os.path.abspath(path)): scipy.misc.imread(path) for path in parameters.files}
-        # print list_tpl_name_image
-        # im = np.random.normal(size=(200, 100))
-        # im[30:50, 60:80] = 20
-        # im2 = np.random.normal(size=(200, 100))
-        # im2[60:100, 20:90] = 10
-        # list_tpl_name_image = dict((
-        #     ("Image1", im),
-        #     ("Image2", im2),
-        #     ("3", im2 + im),
-        #     ("4", im2 - im),
-        # ))
-
+        list_tpl_name_image = {(path if parameters.use_short_path else os.path.abspath(path)): scipy.misc.imread(path)
+                               for path in parameters.files}
 
         app = QtGui.QApplication.instance()
         if not app:
             app = QtGui.QApplication([])
-        win = MyWindow2(dict_name_image=list_tpl_name_image, default_path_layout_file="./layout.dat")
+        win = DViewerWindow(dict_name_image=list_tpl_name_image, default_path_layout_file="./layout.dat")
         win.show()
         QtGui.QApplication.instance().exec_()
