d171c5224890f9ee33092a9a267148bdeb28484a
Sergey Matuynin <sbmatyunin@gmail.com>
2015-06-11 01:13:41 +0300

    Updated draft of mv filtering debugging script. Single function to run, more visualizations.

--- drafts/mv_filtering_demo.py
+++ drafts/mv_filtering_demo.py
@@ -41,82 +41,6 @@ import cv2
 
 # In[4]:
 
-algo.fastdeepflow = reload(algo.fastdeepflow)
-
-
-# In[5]:
-
-img0 = utils.image.imread('mv_filtering/20_0/img0.png')
-img1 = utils.image.imread('mv_filtering/20_0/img1.png')
-
-
-img0 = utils.image.imread('mv_filtering/-48_0/img0.png')
-img1 = utils.image.imread('mv_filtering/-48_0/img1.png')
-
-params = algo.fastdeepflow.create_params("middlebury")
-params.min_size = 10  
-
-
-# In[6]:
-
-dpt0_real = cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY)
-t1 = dpt0_real.reshape(dpt0_real.shape+(1,))
-t2 = img0
-
-dpt0 = numpy.dstack([t2,t1])
-# plt.imshow(dpt0[:,:,0:3])
-x = dpt0[:,:,0:3]
-plt.imshow(img0)
-print dpt0.shape, dpt0.min(), x.min()
-
-numpy.array_equal(dpt0[:,:,0:3],img0)
-
-
-# In[7]:
-
-flow_fwd = algo.fastdeepflow.calc_flow(img1, img0, params)
-flow_bwd = algo.fastdeepflow.calc_flow(img0, img1, params)
-
-
-# In[8]:
-
-plt.figure(figsize=(10,10))
-plt.subplot(1,2,1)
-plt.imshow(img1)
-vh.plot_optical_flow(*flow_fwd)
-
-plt.subplot(1,2,2)
-plt.imshow(img0)
-vh.plot_optical_flow(*flow_bwd)
-
-
-# In[9]:
-
-import algo.motion_analysis.occlusion_processing
-algo.motion_analysis.occlusion_processing = reload(algo.motion_analysis.occlusion_processing)
-import algo.motion_analysis.occlusion_processing as op
-
-dpt0_warped_fixed = op.compensate_and_fix_occlusions(img0, img1, flow_fwd, flow_bwd, dpt0, th_occl=0.9)
-print dpt0_warped_fixed.shape
-plt.imshow(dpt0_warped_fixed[:,:,3].astype('uint8'))
-
-
-# In[10]:
-
-import tests.test_dp_color_mapping
-
-img, dpt, dpt_gt = tests.test_dp_color_mapping.load_data(list_idx_frame_as_start=[0,9])
-
-
-# In[11]:
-
-print dpt[0].shape
-plt.imshow(dpt_gt[1])
-plt.imshow(img[0])
-
-
-# In[12]:
-
 import algo.motion_analysis.occlusion_processing
 algo.motion_analysis.occlusion_processing = reload(algo.motion_analysis.occlusion_processing)
 import algo.motion_analysis.occlusion_processing as op
@@ -124,6 +48,9 @@ import algo.motion_analysis.occlusion_processing as op
 block_size=20
 
 def block_histo_choose1(map_occl, img1, list_img0_warped, list_dpt0_warped, th_num_occl, return_intermediate=False):
+    img = 0
+#     print map_occl.shape, [img.shape for img in list_img0_warped]
+    
     logger = logging.getLogger(__name__)
     dpt_res = numpy.zeros_like(list_dpt0_warped[0])
 
@@ -163,11 +90,13 @@ def block_histo_choose1(map_occl, img1, list_img0_warped, list_dpt0_warped, th_n
 
             def calc_hist(img0_block):
                 imgs = [img0_block[:, :, i].astype('uint8') for i in range(3)]
+#                 print map_occl_block.shape, [img.shape for img in imgs]
                 hist = [cv2.calcHist([img], [0, ],  map_occl_block.astype('uint8'), [8,], [0, 256], ) for img in imgs]
                 hist = numpy.vstack(hist)
                 hist = cv2.normalize(hist)
                 return hist
 
+            
             hist = calc_hist(img1_block)
             list_hist = map(calc_hist, list_img0_warped_block)
 
@@ -195,132 +124,30 @@ def block_histo_choose1(map_occl, img1, list_img0_warped, list_dpt0_warped, th_n
     return dpt_res, dict(map_choice=map_choice)
 
 
-# In[13]:
-
-s_img0 = img[3]
-s_img1 = img[4]
-s_dpt0 = dpt_gt[3]
-s_dpt1 = dpt_gt[4]
-s_flow_fwd = algo.fastdeepflow.calc_flow(s_img1, s_img0)
-s_flow_bwd = algo.fastdeepflow.calc_flow(s_img0, s_img1)
-s_dpt0_warped_fixed, interm = op.compensate_and_fix_occlusions(s_img0, s_img1, s_flow_fwd, s_flow_bwd, s_dpt0, 
-                                                       th_occl=0.9, 
-                                                       return_intermediate=True, 
-                                                       func_block_histo_choose=block_histo_choose1)
-s_dpt0_warped = algo.fastdeepflow.warp_image(s_dpt0, *s_flow_fwd)
-
-
-# In[14]:
-
-interm.keys(), interm['data_block_choose']['map_choice']
-
-
-# In[15]:
-
-# list_flow = interm['interm_dpt_candidates']['list_flow']
-# numpy.array(list_flow).shape
-# list_flow_u = numpy.array(list_flow)[:,0,:,:]
-# list_flow_v = numpy.array(list_flow)[:,1,:,:]
-
-
-# # list_flow_u[map_choise].shape
+# In[ ]:
 
 
 
-# choice_u, choice_v = list_flow_u[map_choise[iy, ix], iy, ix], list_flow_v[map_choise[iy, ix], iy, ix]
-# du, dv = choice_u-list_flow[0][0], choice_v-list_flow[0][1]
 
-# fig, ax = plt.subplots(subplot_kw=dict(axisbg='#EEEEEE'))
-# ax.imshow(map_choise, vmin=0, vmax=4)
-# # plt.imshow(du**2+dv**2)
-# # vh.plot_optical_flow(choice_u, choice_v)
-# vh.plot_optical_flow(*list_flow[1])
-# # vh.plot_optical_flow(du, dv)
+# In[5]:
 
-
-# In[16]:
-
-def get_flow_choice(imterm):
+def get_flow_choice(interm):
     list_flow = interm['interm_dpt_candidates']['list_flow']
     map_choise = interm['data_block_choose']['map_choice']
     ny, nx = map_choise.shape
     ix, iy = numpy.meshgrid(numpy.arange(nx), numpy.arange(ny))
     flow_choice = numpy.array(list_flow)[map_choise[iy, ix], :,  iy, ix]
-    flow_choice = (flow_choice[0], flow_choice[1])
+#     print flow_choice.shape
+    flow_choice = (flow_choice[:,:,0], flow_choice[:,:,1])
     return flow_choice
 
-map_choise = interm['data_block_choose']['map_choice']
-fig, ax = plt.subplots(1,2, subplot_kw=dict(axisbg='#EEEEEE'))
-ax.flat[0].imshow(map_choise, vmin=0, vmax=4)
-ax.flat[1].imshow(map_choise==3, vmin=0, vmax=4)
 
-
-# In[17]:
-
-# vh = reload(utils.visualization_helpers)
-# fig, ax = plt.subplots(2, 2, figsize=(8, 8),sharex=True, sharey=True)
-# fig.subplots_adjust(hspace=0.0)
-# ax.flat[0].imshow(img[0])
-# ax.flat[1].imshow(img[1])
-# ax.flat[2].imshow(map_choise)
-# vh.plot_optical_flow(*list_flow[1], axes=ax.flat[2])
-# # plt.savefig("123123.png")
-
-
-# In[18]:
-
-def plot1(img):
-    
-    ax.flat[plot1.count].imshow(img)
-    plot1.count += 1
-
-plot1.count = 0   
-
-
-# In[19]:
-
-# fig, ax = plt.subplots(6, 2 , figsize=(10, 20),sharex=True, sharey=True)
-# list_flow = interm['interm_dpt_candidates']['list_flow']
-
-
-# xx = [c.copy() for c in s_flow_bwd]
-# xx[0][100:110,100:110] = -100
-# xx[1][100:110,100:110] = -100
-
-# ax.flat[0].imshow(s_img0)
-# vh.plot_optical_flow(*s_flow_bwd, stepu=2, axes=ax.flat[0])
-
-# ax.flat[1].imshow(s_img1)
-# vh.plot_optical_flow(*s_flow_fwd, stepu=2, axes=ax.flat[1])
-
-# ax.flat[2].imshow(s_dpt0)
-# ax.flat[3].imshow(s_dpt1)
-
-
-# start_idx_vis = 4
-# for idx in range(5):
-#     ax_c = ax.flat[start_idx_vis+idx]
-#     ax_c.imshow(interm['list_img0_warped'][idx].astype('uint8'))
-#     vh.plot_optical_flow(*list_flow[idx], stepu=2, axes=ax_c)
-
-# ax.flat[9].imshow(utils.image.abs_diff(s_dpt0_warped, s_dpt0_warped_fixed))
-
-# choice_flow = get_flow_choice(interm)
-# vh.plot_optical_flow(*choice_flow, axes=ax.flat[9])
-# ax.flat[10].imshow(interm['map_occl'])
-# ax.flat[11].imshow(interm['list_dpt0_warped'][1])
-
-
-
-# In[20]:
-
-#fig, ax = plt.subplots(6, 2 , figsize=(10, 20),sharex=True, sharey=True)
-list_flow = interm['interm_dpt_candidates']['list_flow']
-
-def sf(path, img, flow=None):
+def sf_default(dir_base, path, img, flow=None, kwargs_imshow=dict()):
     assert(path is not None)
-    plt.figure(figsize=(20,20), dpi=600)
-    plt.imshow(img)
+    path = os.path.join(dir_base, path)
+    
+    plt.figure(figsize=(20,20), dpi=300)
+    plt.imshow(img, **kwargs_imshow)
     if flow:
         vh.plot_optical_flow(*flow)
     directory = os.path.dirname(path)
@@ -328,56 +155,98 @@ def sf(path, img, flow=None):
         os.makedirs(directory)
     plt.savefig(path)
     plt.close()
+
+def generate_debug(s_img0, s_img1, s_dpt0, s_dpt1, name):
+    s_flow_fwd = algo.fastdeepflow.calc_flow(s_img1, s_img0)
+    s_flow_bwd = algo.fastdeepflow.calc_flow(s_img0, s_img1)
     
-sf("sintel_crop/s_img0_flow_bwd.png", s_img0, s_flow_bwd)
-sf("sintel_crop/s_img0.png", s_img0)
-sf("sintel_crop/s_img1_flow_fwd.png", s_img1, s_flow_fwd)
-sf("sintel_crop/s_img1.png", s_img1)
+    s_dpt0_warped = algo.fastdeepflow.warp_image(s_dpt0, *s_flow_fwd)
+    
+    s_dpt0_warped_fixed, interm = op.compensate_and_fix_occlusions(s_img0, s_img1, s_flow_fwd, s_flow_bwd, s_dpt0, 
+                                                           th_occl=0.9, 
+                                                           return_intermediate=True, 
+                                                           func_block_histo_choose=block_histo_choose1)
+    
+    
+    
+    map_choise = interm['data_block_choose']['map_choice']
+    choice_flow = get_flow_choice(interm)
+    
+    def sf(*args, **kwargs):
+        return sf_default("dump/"+name+"/", *args, **kwargs)
+    
+    sf("img0_flow_bwd.png", s_img0, s_flow_bwd)
+    sf("img0.png", s_img0)
+    sf("img1_flow_fwd.png", s_img1, s_flow_fwd)
+    sf("img1.png", s_img1)
 
-sf("sintel_crop/s_dpt0.png", s_dpt0)
-sf("sintel_crop/s_dpt1.png", s_dpt1)
+    sf("dpt0.png", s_dpt0)
+    sf("dpt1.png", s_dpt1)
 
-start_idx_vis = 4
-for idx in range(5):
-    sf('sintel_crop/img0_warped_{}_flow.png'.format(idx), 
-       interm['list_img0_warped'][idx].astype('uint8'),
-       list_flow[idx])
+
+    start_idx_vis = 4
+    list_flow = interm['interm_dpt_candidates']['list_flow']
+    for idx in range(5):
+        sf('img0_warped_{}_flow.png'.format(idx), 
+           interm['list_img0_warped'][idx].astype('uint8'),
+           list_flow[idx])
+
+    sf("map_choice.png", map_choise, kwargs_imshow=dict(vmin=0, vmax=4))
+    sf("map_choice_flow.png", map_choise, flow=choice_flow, kwargs_imshow=dict(vmin=0, vmax=4))
+
+    sf("map_occl.png", interm['map_occl'])
+    sf("occl.png", interm['occl'])
+
+    sf("map_occl_flow_bwd.png", interm['map_occl'], flow=s_flow_bwd)
+    sf("occl_flow_bwd.png", interm['occl'], flow=s_flow_bwd)
+
+    sf("map_occl_flow_fwd.png", interm['map_occl'], flow=s_flow_fwd)
+    sf("occl_flow_fwd.png", interm['occl'], flow=s_flow_fwd)
+    
+
+
+# In[6]:
+
+import tests.test_dp_color_mapping
+
+img, dpt, dpt_gt = tests.test_dp_color_mapping.load_data(list_idx_frame_as_start=[0,9])
+img0, img1 = img[3], img[4]
+dpt0, dpt1 = dpt_gt[3], dpt_gt[4]
+# generate_debug(img0, img1, dpt0, dpt1, "sintel_crop")
+# print "img1.shape", img1.shape
+generate_debug(img1, img0, dpt1, dpt0, "sintel_crop_reverse")
+
+
+# In[ ]:
+
+
+
+
+
+# In[33]:
+
+# vh.plot_optical_flow(*choice_flow)
+# choice_flow
+
+
+# In[32]:
+
+
+    
+
+
 
 # ax.flat[9].imshow(utils.image.abs_diff(s_dpt0_warped, s_dpt0_warped_fixed))
 
-# choice_flow = get_flow_choice(interm)
+# 
 # vh.plot_optical_flow(*choice_flow, axes=ax.flat[9])
-# ax.flat[10].imshow(interm['map_occl'])
+# imshow()
 # ax.flat[11].imshow(interm['list_dpt0_warped'][1])
 
 
-# In[21]:
+# In[29]:
 
-utils.flow.flow_equal(s_dpt0_warped, )
-
-
-# In[ ]:
-
-123
-
-
-# In[ ]:
-
-# from PyQt4 import QtGui, QtCore
-
-# class MyQtWindow(QtGui.QMainWindow):
-#     # [...] your Qt window code
-#     pass
-
-# # app = QtGui.QApplication(["asdasd"])
-# window = MyQtWindow()
-# # window.show
-# # app.exec_()
-
-
-# In[ ]:
-
-123
+interm.keys()
 
 
 # In[ ]:
